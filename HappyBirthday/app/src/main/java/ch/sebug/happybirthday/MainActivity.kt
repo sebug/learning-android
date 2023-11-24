587ca938c5d543c0dd97b28f52f492291cff60af
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-11-24 14:59:38 +0100

    Birthday card project

--- HappyBirthday/app/src/main/java/ch/sebug/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/ch/sebug/happybirthday/MainActivity.kt
@@ -1,31 +1,28 @@
-package ch.sebug.greetingcard
+package ch.sebug.happybirthday
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import ch.sebug.greetingcard.ui.theme.GreetingCardTheme
+import ch.sebug.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GreetingCardTheme {
+            HappyBirthdayTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting(name = "Seb")
+                    Greeting("Android")
                 }
             }
         }
@@ -34,18 +31,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface (color = Color.Cyan) {
-        Text(
-            text = "Hello, my name is $name!",
-            modifier = modifier.padding(24.dp)
-        )
-    }
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    GreetingCardTheme {
-        Greeting(name = "Sebastian")
+fun BirthdayCardPreview() {
+    HappyBirthdayTheme {
+        Greeting("Android")
     }
 }
