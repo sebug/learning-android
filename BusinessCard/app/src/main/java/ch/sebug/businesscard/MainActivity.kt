1ae2c2eca433f2ef2deedda4315ff10ee146ace6
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-11-28 10:04:32 +0100

    Adding heading

--- BusinessCard/app/src/main/java/ch/sebug/businesscard/MainActivity.kt
+++ BusinessCard/app/src/main/java/ch/sebug/businesscard/MainActivity.kt
@@ -3,13 +3,24 @@ package ch.sebug.businesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ch.sebug.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,10 +46,29 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BusinessCard(contactInformation: ContactInformation, modifier: Modifier = Modifier) {
-    Text(
-        text = contactInformation.fullName,
-        modifier = modifier
-    )
+    Column {
+        Heading(contactInformation = contactInformation)
+    }
+}
+
+@Composable
+fun Heading(contactInformation: ContactInformation, modifier: Modifier = Modifier) {
+    val picturePainter = painterResource(id = R.drawable.android_logo)
+    Column(modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(painter = picturePainter,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = modifier.size(200.dp)
+        )
+        Text(text = contactInformation.fullName,
+            fontSize = 36.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = modifier.fillMaxWidth())
+        Text(text = contactInformation.title,
+            fontSize = 24.sp)
+    }
 }
 
 @Preview(showBackground = true)
