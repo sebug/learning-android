f343a7c17a7e5a4e1408ebc8cb30fe04460f2d6c
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-04 17:03:32 +0100

    Querying cat API

---
+++ cat-api-fetcher/index.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+const {mkdir,writeFile} = require("fs/promises");
+const { Readable } = require('stream');
+const { finished } = require('stream/promises');
+const path = require("path");
+
+const downloadImage = async (url, fileName) => {
+    console.log('Downloading ' + url + ' to ' + fileName);
+    const res = await fetch(url);
+    const destination = path.resolve('.', fileName)
+    console.log('destination being ' + destination);
+    const fileStream = fs.createWriteStream(destination, { flags: 'wx' });
+    await finished(Readable.fromWeb(res.body).pipe(fileStream));
+};
+
+const getAndSaveRandomImages = async () => {
+    const imagesResponse = await fetch('https://api.thecatapi.com/v1/images/search?limit=10');
+    if (imagesResponse.status !== 200) {
+	throw new Error('Could not get the cats - status ' + imagesResponse.status);
+    }
+    const imagesJson = await imagesResponse.json();
+    let i = 0;
+    for (const imageReference of imagesJson) {
+	const extensionPoint = imageReference.url.lastIndexOf('.');
+	const extension = imageReference.url.substring(extensionPoint + 1);
+	const fileName = 'cat_' + i + '.' + extension;
+	await downloadImage(imageReference.url, fileName);
+	i += 1;
+    }
+}
+
+getAndSaveRandomImages();
