26e1c5f7d1ff2b8bf3af247652a0ea37483fc937
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-04 17:26:52 +0100

    Affirmation list

--- Affirmations/app/src/main/java/ch/sebug/affirmations/MainActivity.kt
+++ Affirmations/app/src/main/java/ch/sebug/affirmations/MainActivity.kt
@@ -3,13 +3,28 @@ package ch.sebug.affirmations
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ch.sebug.affirmations.data.Datasource
+import ch.sebug.affirmations.model.Affirmation
 import ch.sebug.affirmations.ui.theme.AffirmationsTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    AffirmationsApp()
                 }
             }
         }
@@ -30,17 +45,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AffirmationsApp() {
+    AffirmationList(affirmationList = Datasource().loadAffirmations())
+}
+
+@Composable
+fun AffirmationCard(affirmation: Affirmation,
+                    modifier: Modifier = Modifier) {
+    Card(modifier = modifier) {
+        Column {
+             Image(painter = painterResource(affirmation.imageResourceId),
+                 contentDescription = stringResource(affirmation.stringResourceId),
+                 modifier = Modifier
+                     .fillMaxWidth()
+                     .height(164.dp),
+                 contentScale = ContentScale.Crop)
+            Text(text = LocalContext.current.getString(affirmation.stringResourceId),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.headlineSmall)
+        }
+    }
+}
+
+@Composable
+fun AffirmationList(affirmationList: List<Affirmation>,
+                    modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(affirmationList) {
+            affirmation ->
+                AffirmationCard(affirmation = affirmation,
+                    modifier = Modifier.padding(8.dp))
+
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun AffirmationCardPreview() {
+    AffirmationCard(Affirmation(R.string.affirmation_0, R.drawable.cat_0))
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun AffirmationsPreview() {
     AffirmationsTheme {
-        Greeting("Android")
+        AffirmationsApp()
     }
 }
