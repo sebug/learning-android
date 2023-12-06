96999b229b1b3288462c73aec2839c5d754fa730
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-06 12:37:56 +0100

    Works with colors

--- Meow/app/src/main/java/ch/sebug/meow/ui/theme/Theme.kt
+++ Meow/app/src/main/java/ch/sebug/meow/ui/theme/Theme.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import androidx.compose.material3.Shapes as Shapes
 
 private val LightColors = lightColorScheme(
     primary = md_theme_light_primary,
@@ -82,17 +83,31 @@ private val DarkColors = darkColorScheme(
 
 @Composable
 fun MeowTheme(
-    useDarkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = false,
     content: @Composable() () -> Unit
 ) {
-    val colors = if (!useDarkTheme) {
-        LightColors
-    } else {
-        DarkColors
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColors
+        else -> LightColors
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
     }
 
     MaterialTheme(
-        colorScheme = colors,
+        colorScheme = colorScheme,
+        typography = Typography,
         content = content
     )
 }
