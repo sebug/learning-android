dac831655757ed1eaeb8b97e6f07c055318ccfdd
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-06 15:06:36 +0100

    State in icon button

--- Meow/app/src/main/java/ch/sebug/meow/MainActivity.kt
+++ Meow/app/src/main/java/ch/sebug/meow/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -150,7 +151,12 @@ private fun CatItemButton(
     IconButton(onClick = onClick,
         modifier = modifier) {
         Icon(
-            imageVector = Icons.Filled.ExpandMore,
+            imageVector =
+            if (expanded) {
+                Icons.Filled.ExpandLess
+            } else {
+                Icons.Filled.ExpandMore
+            },
             contentDescription = stringResource(R.string.expand_button_content_description),
             tint = MaterialTheme.colorScheme.secondary
         )
