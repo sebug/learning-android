a06da71ca9181a7ba1b44ded88bc3a95833a0609
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-06 12:23:13 +0100

    Lazy column

--- Meow/app/src/main/java/ch/sebug/meow/MainActivity.kt
+++ Meow/app/src/main/java/ch/sebug/meow/MainActivity.kt
@@ -3,13 +3,28 @@ package ch.sebug.meow
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import ch.sebug.meow.data.Cat
+import ch.sebug.meow.data.cats
 import ch.sebug.meow.ui.theme.MeowTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MeowApp()
                 }
             }
         }
@@ -30,17 +45,64 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun MeowApp() {
+    LazyColumn {
+        items(cats) {
+            CatItem(cat = it)
+        }
+    }
+}
+
+@Composable
+fun CatItem(cat: Cat,
+            modifier: Modifier = Modifier) {
+    Row(
         modifier = modifier
+            .fillMaxWidth()
+            .padding(dimensionResource(R.dimen.padding_small))
+    ) {
+        CatIcon(cat.imageResourceId)
+        CatInformation(cat.name, cat.age)
+    }
+}
+
+@Composable
+fun CatIcon(@DrawableRes catIcon: Int,
+            modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier
+            .size(dimensionResource(R.dimen.image_size))
+            .padding(dimensionResource(R.dimen.padding_small)),
+        painter = painterResource(catIcon),
+
+        // Content Description is not needed here - image is decorative, and setting a null content
+        // description allows accessibility services to skip this element during navigation.
+
+        contentDescription = null
     )
+}
+
+@Composable
+fun CatInformation(
+    @StringRes catName: Int,
+    catAge: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        Text(
+            text = stringResource(catName),
+            modifier = Modifier.padding(top = dimensionResource(R.dimen.padding_small))
+        )
+        Text(
+            text = stringResource(R.string.years_old, catAge),
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MeowAppPreview() {
     MeowTheme {
-        Greeting("Android")
+        MeowApp()
     }
 }
