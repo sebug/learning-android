cca9ed0b950858309f94b83a9700916d5462e01a
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-06 23:00:39 +0100

    Game logic works

--- Unscramble/app/src/main/java/ch/sebug/unscramble/MainActivity.kt
+++ Unscramble/app/src/main/java/ch/sebug/unscramble/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import ch.sebug.unscramble.ui.GameScreen
 import ch.sebug.unscramble.ui.theme.UnscrambleTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +23,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    GameScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    UnscrambleTheme {
-        Greeting("Android")
-    }
-}
