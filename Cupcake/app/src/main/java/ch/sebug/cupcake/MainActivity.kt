7f51f38466dc502e43f050898942d7321a10ac07
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-07 11:41:53 +0100

    Main activity adapted

--- Cupcake/app/src/main/java/ch/sebug/cupcake/MainActivity.kt
+++ Cupcake/app/src/main/java/ch/sebug/cupcake/MainActivity.kt
@@ -10,37 +10,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import ch.sebug.cupcake.ui.theme.CupcakeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             CupcakeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                CupcakeApp()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CupcakeTheme {
-        Greeting("Android")
     }
 }
