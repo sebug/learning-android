e2e28a91e8ba8d3f756df006bfc4342fae8e806d
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-07 14:41:10 +0100

    Asserting that it is start screen initially

---
+++ Cupcake/app/src/androidTest/java/ch/sebug/cupcake/test/CupcakeScreenNavigationTest.kt
@@ -0,0 +1,35 @@
+package ch.sebug.cupcake.test
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import ch.sebug.cupcake.CupcakeApp
+import ch.sebug.cupcake.CupcakeScreen
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class CupcakeScreenNavigationTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    private lateinit var navController: TestNavHostController
+
+    @Before
+    fun setupCupcakeNavHost() {
+        composeTestRule.setContent {
+            navController = TestNavHostController(LocalContext.current).apply {
+                navigatorProvider.addNavigator(ComposeNavigator())
+            }
+            CupcakeApp(navController = navController)
+        }
+    }
+
+    @Test
+    fun cupcakeNavHost_verifyStartDestination() {
+        assertEquals(CupcakeScreen.Start .name, navController.currentBackStackEntry?.destination?.route)
+    }
+}
