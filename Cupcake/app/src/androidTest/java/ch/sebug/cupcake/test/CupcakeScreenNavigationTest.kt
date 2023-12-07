af252754caee2a856a1abcada91780371c6b32a1
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-07 14:47:49 +0100

    On node with text extension method

--- Cupcake/app/src/androidTest/java/ch/sebug/cupcake/test/CupcakeScreenNavigationTest.kt
+++ Cupcake/app/src/androidTest/java/ch/sebug/cupcake/test/CupcakeScreenNavigationTest.kt
@@ -30,6 +30,6 @@ class CupcakeScreenNavigationTest {
 
     @Test
     fun cupcakeNavHost_verifyStartDestination() {
-        assertEquals(CupcakeScreen.Start .name, navController.currentBackStackEntry?.destination?.route)
+        navController.assertCurrentRouteName(CupcakeScreen.Start.name)
     }
 }
