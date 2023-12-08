cac030d2e194f8daf3b94561affdf8fb596ec064
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-08 12:00:38 +0100

    Mars photos app

---
+++ MarsPhotos/app/src/main/java/ch/sebug/marsphotos/ui/MarsPhotosApp.kt
@@ -0,0 +1,55 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package ch.sebug.marsphotos.ui
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import ch.sebug.marsphotos.R
+import ch.sebug.marsphotos.ui.screens.HomeScreen
+import ch.sebug.marsphotos.ui.screens.MarsViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MarsPhotosApp() {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = { MarsTopAppBar(scrollBehavior = scrollBehavior) }
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            val marsViewModel: MarsViewModel = viewModel()
+            HomeScreen(
+                marsUiState = marsViewModel.marsUiState)
+        }
+    }
+}
+
+@Composable
+fun MarsTopAppBar(scrollBehavior: TopAppBarScrollBehavior, modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        scrollBehavior = scrollBehavior,
+        title = {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.headlineSmall,
+            )
+        },
+        modifier = modifier
+    )
+}
