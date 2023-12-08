bde34ab2dbe2252e6a8d08088a5108b5918f0c19
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-08 12:01:45 +0100

    Mars app launches

--- MarsPhotos/app/src/main/java/ch/sebug/marsphotos/MainActivity.kt
+++ MarsPhotos/app/src/main/java/ch/sebug/marsphotos/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import ch.sebug.marsphotos.ui.MarsPhotosApp
 import ch.sebug.marsphotos.ui.theme.MarsPhotosTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +23,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MarsPhotosApp()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MarsPhotosTheme {
-        Greeting("Android")
-    }
-}
