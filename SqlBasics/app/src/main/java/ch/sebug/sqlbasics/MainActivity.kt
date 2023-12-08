4b41136629e29edd9042ffbb636fbfbbcaf061e7
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-08 19:05:03 +0100

    Adding database

--- SqlBasics/app/src/main/java/ch/sebug/sqlbasics/MainActivity.kt
+++ SqlBasics/app/src/main/java/ch/sebug/sqlbasics/MainActivity.kt
@@ -3,11 +3,13 @@ package ch.sebug.sqlbasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ch.sebug.sqlbasics.ui.theme.SQLBasicsTheme
@@ -22,25 +24,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text("The database is ready!")
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SQLBasicsTheme {
-        Greeting("Android")
-    }
-}
