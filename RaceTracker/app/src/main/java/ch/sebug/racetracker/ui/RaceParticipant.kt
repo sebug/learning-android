80ca0df10ab700f256118506a342106e1dd6d763
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-08 10:50:37 +0100

    Unit tests with time

--- RaceTracker/app/src/main/java/ch/sebug/racetracker/ui/RaceParticipant.kt
+++ RaceTracker/app/src/main/java/ch/sebug/racetracker/ui/RaceParticipant.kt
@@ -1,8 +1,10 @@
 package ch.sebug.racetracker.ui
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.delay
 
 /**
@@ -28,9 +30,14 @@ class RaceParticipant(
 
 
     suspend fun run() {
-        while (currentProgress < maxProgress) {
-            delay(progressDelayMillis)
-            currentProgress += progressIncrement
+        try {
+            while (currentProgress < maxProgress) {
+                delay(progressDelayMillis)
+                currentProgress += progressIncrement
+            }
+        } catch (e: CancellationException) {
+            Log.e("RaceParticipant", "$name: ${e.message}")
+            throw e
         }
     }
 
---
+++ RaceTracker/app/src/test/java/ch/sebug/racetracker/RaceParticipantTest.kt
@@ -0,0 +1,24 @@
+package ch.sebug.racetracker
+
+import ch.sebug.racetracker.ui.RaceParticipant
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.advanceTimeBy
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class RaceParticipantTest {
+    private val raceParticipant = RaceParticipant("Runner 1")
+
+    @Test
+    fun raceParticipant_RaceStarted_ProgressUpdated() = runTest {
+        val expectedProgress = 1
+        launch {
+            raceParticipant.run()
+        }
+        advanceTimeBy(raceParticipant.progressDelayMillis)
+        runCurrent()
+        assertEquals(expectedProgress, raceParticipant.currentProgress)
+    }
+}
