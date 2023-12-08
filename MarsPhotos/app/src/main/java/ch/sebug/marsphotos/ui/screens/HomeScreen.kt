0d7d7359e097f94a8ed04cf837bfc4f51fdd58bc
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-08 12:44:42 +0100

    Failed and error screen

--- MarsPhotos/app/src/main/java/ch/sebug/marsphotos/ui/screens/HomeScreen.kt
+++ MarsPhotos/app/src/main/java/ch/sebug/marsphotos/ui/screens/HomeScreen.kt
@@ -1,12 +1,20 @@
 package ch.sebug.marsphotos.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ch.sebug.marsphotos.R
 import ch.sebug.marsphotos.ui.theme.MarsPhotosTheme
 
@@ -14,12 +22,36 @@ import ch.sebug.marsphotos.ui.theme.MarsPhotosTheme
 fun HomeScreen(
     marsUiState: MarsUiState, modifier: Modifier = Modifier
 ) {
-    val message = when(marsUiState) {
-        is MarsUiState.Error -> "Error"
-        is MarsUiState.Loading -> "Loading photos"
-        is MarsUiState.Success -> marsUiState.photos
+    when (marsUiState) {
+        is MarsUiState.Success -> {
+            ResultScreen(photos = marsUiState.photos, modifier = modifier)
+        }
+        is MarsUiState.Loading -> LoadingScreen(modifier = modifier.fillMaxSize())
+        is MarsUiState.Error -> ErrorScreen( modifier = modifier.fillMaxSize())
     }
-    ResultScreen(message, modifier)
+}
+
+@Composable
+fun ErrorScreen(modifier: Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_connection_error), contentDescription = ""
+        )
+        Text(text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp))
+    }
+}
+
+@Composable
+fun LoadingScreen(modifier: Modifier) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading_img),
+        contentDescription = stringResource(R.string.loading)
+    )
 }
 
 /**
