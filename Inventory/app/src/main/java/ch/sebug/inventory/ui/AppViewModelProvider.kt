451dfd48e319208374e525590102bffbb227d4cd
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-11 14:54:18 +0100

    Most UI

---
+++ Inventory/app/src/main/java/ch/sebug/inventory/ui/AppViewModelProvider.kt
@@ -0,0 +1,50 @@
+package ch.sebug.inventory.ui
+
+import android.app.Application
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import ch.sebug.inventory.InventoryApplication
+import ch.sebug.inventory.ui.home.HomeViewModel
+import ch.sebug.inventory.ui.item.ItemDetailsViewModel
+import ch.sebug.inventory.ui.item.ItemEditViewModel
+import ch.sebug.inventory.ui.item.ItemEntryViewModel
+
+/**
+ * Provides Factory to create instance of ViewModel for the entire Inventory app
+ */
+object AppViewModelProvider {
+    val Factory = viewModelFactory {
+        // Initializer for ItemEditViewModel
+        initializer {
+            ItemEditViewModel(
+                this.createSavedStateHandle()
+            )
+        }
+        // Initializer for ItemEntryViewModel
+        initializer {
+            ItemEntryViewModel()
+        }
+
+        // Initializer for ItemDetailsViewModel
+        initializer {
+            ItemDetailsViewModel(
+                this.createSavedStateHandle()
+            )
+        }
+
+        // Initializer for HomeViewModel
+        initializer {
+            HomeViewModel()
+        }
+    }
+}
+
+/**
+ * Extension function to queries for [Application] object and returns an instance of
+ * [InventoryApplication].
+ */
+fun CreationExtras.inventoryApplication(): InventoryApplication =
+    (this[AndroidViewModelFactory.APPLICATION_KEY] as InventoryApplication)
---
+++ Inventory/app/src/main/java/ch/sebug/inventory/ui/navigation/InventoryNavGraph.kt
@@ -0,0 +1,69 @@
+package ch.sebug.inventory.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import ch.sebug.inventory.ui.home.HomeDestination
+import ch.sebug.inventory.ui.home.HomeScreen
+import ch.sebug.inventory.ui.item.ItemDetailsDestination
+import ch.sebug.inventory.ui.item.ItemDetailsScreen
+import ch.sebug.inventory.ui.item.ItemEditDestination
+import ch.sebug.inventory.ui.item.ItemEditScreen
+import ch.sebug.inventory.ui.item.ItemEntryDestination
+import ch.sebug.inventory.ui.item.ItemEntryScreen
+
+/**
+ * Provides Navigation graph for the application.
+ */
+@Composable
+fun InventoryNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
+) {
+    NavHost(
+        navController = navController,
+        startDestination = HomeDestination.route,
+        modifier = modifier
+    ) {
+        composable(route = HomeDestination.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(ItemEntryDestination.route) },
+                navigateToItemUpdate = {
+                    navController.navigate("${ItemDetailsDestination.route}/${it}")
+                }
+            )
+        }
+        composable(route = ItemEntryDestination.route) {
+            ItemEntryScreen(
+                navigateBack = { navController.popBackStack() },
+                onNavigateUp = { navController.navigateUp() }
+            )
+        }
+        composable(
+            route = ItemDetailsDestination.routeWithArgs,
+            arguments = listOf(navArgument(ItemDetailsDestination.itemIdArg) {
+                type = NavType.IntType
+            })
+        ) {
+            ItemDetailsScreen(
+                navigateToEditItem = { navController.navigate("${ItemEditDestination.route}/$it") },
+                navigateBack = { navController.navigateUp() }
+            )
+        }
+        composable(
+            route = ItemEditDestination.routeWithArgs,
+            arguments = listOf(navArgument(ItemEditDestination.itemIdArg) {
+                type = NavType.IntType
+            })
+        ) {
+            ItemEditScreen(
+                navigateBack = { navController.popBackStack() },
+                onNavigateUp = { navController.navigateUp() }
+            )
+        }
+    }
+}
