d11b07585031fe01c3740acd421afa6e57c6eb87
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-11 16:12:56 +0100

    Saving does something

--- Inventory/app/src/main/java/ch/sebug/inventory/ui/item/ItemEntryScreen.kt
+++ Inventory/app/src/main/java/ch/sebug/inventory/ui/item/ItemEntryScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
@@ -27,6 +28,7 @@ import ch.sebug.inventory.R
 import ch.sebug.inventory.ui.AppViewModelProvider
 import ch.sebug.inventory.ui.navigation.NavigationDestination
 import ch.sebug.inventory.ui.theme.InventoryTheme
+import kotlinx.coroutines.launch
 import java.util.Currency
 import java.util.Locale
 
@@ -43,6 +45,7 @@ fun ItemEntryScreen(
     canNavigateBack: Boolean = true,
     viewModel: ItemEntryViewModel = viewModel(factory = AppViewModelProvider.Factory)
 ) {
+    val coroutineScope = rememberCoroutineScope()
     Scaffold(
         topBar = {
             InventoryTopAppBar(
@@ -55,7 +58,12 @@ fun ItemEntryScreen(
         ItemEntryBody(
             itemUiState = viewModel.itemUiState,
             onItemValueChange = viewModel::updateUiState,
-            onSaveClick = { },
+            onSaveClick = {
+                          coroutineScope.launch {
+                              viewModel.saveItem()
+                              navigateBack()
+                          }
+            },
             modifier = Modifier
                 .padding(innerPadding)
                 .verticalScroll(rememberScrollState())
