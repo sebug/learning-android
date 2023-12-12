325db86d9203a109b5dab129228d38a6a8a1b9b6
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-12 18:44:25 +0100

    Additional workers

---
+++ WorkManager/app/src/main/java/ch/sebug/workmanager/workers/CleanupWorker.kt
@@ -0,0 +1,55 @@
+package ch.sebug.workmanager.workers
+
+import android.content.Context
+import android.util.Log
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import ch.sebug.workmanager.DELAY_TIME_MILLIS
+import ch.sebug.workmanager.OUTPUT_PATH
+import ch.sebug.workmanager.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+import java.io.File
+
+/**
+ * Cleans up temporary files generated during blurring process
+ */
+private const val TAG = "CleanupWorker"
+
+class CleanupWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, params) {
+
+    override suspend fun doWork(): Result {
+        /** Makes a notification when the work starts and slows down the work so that it's easier
+         * to see each WorkRequest start, even on emulated devices
+         */
+        makeStatusNotification(
+            applicationContext.resources.getString(R.string.cleaning_up_files),
+            applicationContext
+        )
+
+        return withContext(Dispatchers.IO) {
+            delay(DELAY_TIME_MILLIS)
+
+            return@withContext try {
+                val outputDirectory = File(applicationContext.filesDir, OUTPUT_PATH)
+                if (outputDirectory.exists()) {
+                    val entries = outputDirectory.listFiles()
+                    if (entries != null) {
+                        for (entry in entries) {
+                            val name = entry.name
+                            if (name.isNotEmpty() && name.endsWith(".png")) {
+                                val deleted = entry.delete()
+                                Log.i(TAG, "Deleted $name - $deleted")
+                            }
+                        }
+                    }
+                }
+                Result.success()
+            } catch (exception: Exception) {
+                exception.printStackTrace()
+                Result.failure()
+            }
+        }
+    }
+}
