f2a7e54074592d4f82c1c276204cc57f86a22baa
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-13 10:33:37 +0100

    Asserting blur worker result

--- WorkManager/app/src/androidTest/java/ch/sebug/workmanager/workers/WorkerInstrumentationTest.kt
+++ WorkManager/app/src/androidTest/java/ch/sebug/workmanager/workers/WorkerInstrumentationTest.kt
@@ -3,11 +3,16 @@ package ch.sebug.workmanager.workers
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.matcher.ViewMatchers.assertThat
+import androidx.work.Data
 import androidx.work.ListenableWorker
 import androidx.work.WorkRequest
 import androidx.work.testing.TestListenableWorkerBuilder
+import ch.sebug.workmanager.KEY_BLUR_LEVEL
+import ch.sebug.workmanager.KEY_IMAGE_URI
+import ch.sebug.workmanager.getImageUri
 import kotlinx.coroutines.runBlocking
 import org.hamcrest.CoreMatchers.`is`
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 
@@ -26,6 +31,23 @@ class WorkerInstrumentationTest {
         runBlocking {
             val result = worker.doWork()
             assertThat(result, `is`(ListenableWorker.Result.success()))
+        }
+    }
+
+    @Test
+    fun blurWorker_doWork_resultSuccessReturnsUri() {
+        val imageUri = context.getImageUri()
+        val blurLevel = 1
+        val builder = Data.Builder()
+        builder.putString(KEY_IMAGE_URI, imageUri.toString()).putInt(KEY_BLUR_LEVEL, blurLevel)
+
+        val worker = TestListenableWorkerBuilder<BlurWorker>(context)
+            .setInputData(builder.build())
+            .build()
+        runBlocking {
+            val result = worker.doWork()
+            val resultUri = result.outputData.getString(KEY_IMAGE_URI)
+            assert(resultUri!!.contains(".png"))
         }
     }
 }
