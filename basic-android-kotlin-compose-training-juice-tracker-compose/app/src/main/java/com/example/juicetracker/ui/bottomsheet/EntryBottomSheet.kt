832475c8089aa2d374e34efbf6a68928b32d7d9b
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-18 10:32:58 +0100

    Rating bar

--- basic-android-kotlin-compose-training-juice-tracker-compose/app/src/main/java/com/example/juicetracker/ui/bottomsheet/EntryBottomSheet.kt
+++ basic-android-kotlin-compose-training-juice-tracker-compose/app/src/main/java/com/example/juicetracker/ui/bottomsheet/EntryBottomSheet.kt
@@ -114,6 +114,10 @@ fun SheetForm(
             onColorChange = {color ->
                 onUpdateJuice(juice.copy(color = JuiceColor.values()[color].name))
             })
+        RatingInputRow(rating = juice.rating,
+            onRatingChange = { rating ->
+                onUpdateJuice(juice.copy(rating = rating))
+            })
         ButtonRow(
             modifier = Modifier.align(Alignment.CenterHorizontally),
             onCancel = onCancel,
